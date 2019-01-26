3411fed980e2caa1c347590219125485fbec805d
bgtktsc <gtktsc@gmail.com>
2019-01-26 16:19:16 +0100

    init/6: add enzyme

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,8 @@ module.exports = {
         '^.+\\.tsx?$': 'ts-jest'
     },
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    collectCoverage: true,
+    coverageReporters: ['json'],
+    setupFiles: ['./setupTests.ts']
 };
